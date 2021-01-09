41d8fcd9f458053787c142c7b1f368f2d3cfaa9f
devrockingout <econt037@uottawa.ca>
2021-01-08 21:21:46 -0500

    added node states colors

--- NodeComponent.js
+++ NodeComponent.js
@@ -8,6 +8,13 @@ function NodeComponent(){
     this.component = null;
     this.name = null;
     this.position = { x:"0px", y:"0px" };
+
+    this.colors = { 
+        green: "#2FBF71", 
+        red: "#DD1C1A",
+        orange: "#FF9505", 
+        violet: "#7B4B94"
+    };
 }
 
 /**
@@ -19,7 +26,7 @@ NodeComponent.prototype.create = function(name){
     this.name = name;
 
     this.component = '<svg width="200" height="200" id ="'+name+'" class="node" style="left: ' + this.position.x + '; top: ' + this.position.y + ';" onclick="nodeClick(this.id)"> ';
-    this.component += '<g><circle cx="100" cy="100" r="80" fill="red" />';
+    this.component += '<g><circle cx="100" cy="100" r="80" fill="'+ this.colors.green +'" />';
     this.component += '<text x="50%" y="50%" text-anchor="middle" dy=".3em" class="textSvg">'+name+'</text></g>';
     this.component += '<path class="path1" d="M 20 100 A 80 80 0 0 1 180 100" fill ="none"  stroke="black" stroke-width="4" />';
     this.component += '<path class="path2" d="M 180 100 A 80 80 0 0 1 20 100" fill ="none"  stroke="black" stroke-width="4" />';
